1eddc76074382b15d060161865fa0e57caeac259
Wyatt92 <vi_hoang@hotmail.de>
2017-09-06 19:37:39 +0200

    Tutorial 8 - DrawFace - DrawPoo - ClampScreenX - ClampScreenY - Movement

--- Chili Framework 2016/Engine/Game.h
+++ Chili Framework 2016/Engine/Game.h
@@ -36,11 +36,28 @@ private:
 	void UpdateModel();
 	/********************************/
 	/*  User Functions              */
+	void DrawFace(int x, int y);
+	void DrawPoo(int x, int y);
+	int ClampScreenX(int x, int width);
+	int ClampScreenY(int y, int height);
 	/********************************/
 private:
 	MainWindow& wnd;
 	Graphics gfx;
 	/********************************/
 	/*  User Variables              */
+	int dudeX = 400;
+	int dudeY = 300;
+	int dudeWidth = 20;
+	int dudeHeight = 20;
+	int poo0X = 300;
+	int poo0Y = 150;
+	bool poo0IsEaten = false;
+	int poo1X = 100;
+	int poo1Y = 500;
+	bool poo1IsEaten = false;
+	int poo2X = 600;
+	int poo2Y = 400;
+	bool poo2IsEaten = false;
 	/********************************/
 };
